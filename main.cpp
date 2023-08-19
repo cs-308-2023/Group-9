50384371b78c54dc0f37ad7f9e6d6fba5f54a813
Sandeep <sandeepiitm2003@gmail.com>
2023-08-19 14:55:04 +0530

    third commit

--- main.cpp
+++ main.cpp
@@ -4,10 +4,12 @@ using namespace std;
 int main(){
     print_hello();
     cout<< endl;
+    cout << "Calculating the factorial program"<< endl;
     cout << "enter a number";
     int n; cin>> n;
     int fact = factorial(n);
     if(fact > 0) cout << "Factorial of "<< n << "is"<< factorial(n)<< endl;
     else cout << "kindly enter positive number\n";
+    
     return 0;
 }
