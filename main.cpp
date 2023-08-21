3109a2c8568e7f5bb4a5c9ba81ab92155910edee
Sandeep <sandeepiitm2003@gmail.com>
2023-08-21 14:45:58 +0530

    changed cout

--- main.cpp
+++ main.cpp
@@ -3,13 +3,13 @@
 using namespace std;
 int main(){
     print_hello();
-    cout<< endl;
-    cout << "Calculating the factorial program"<< endl;
-    cout << "enter a number";
+    printf("\n");
+    printf("Calculating the factorial program\n");
+    printf("enter a number\n");
     int n; cin>> n;
     int fact = factorial(n);
     if(fact > 0) cout << "Factorial of "<< n << "is"<< factorial(n)<< endl;
-    else cout << "kindly enter positive number\n";
+    else printf("kindly enter positive number\n");
     
     return 0;
 }
